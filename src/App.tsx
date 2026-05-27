ebc560a50d3cf73b527a3bd03b1957c40a7243ef
ssk294 <fy69bz45gc@privaterelay.appleid.com>
2026-05-27 11:39:11 +0900

    検索窓の追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,27 @@
 import './index.css';
 
+import { useState } from 'react';
 export default function App() {
   const gameTitle = 'おすすめのゲーム検索🎮';
+  const [searchTerm, setSerchTerm] = useState('');
+
+  const handleSearch = () =>{
+    alert('検索ワード: ' + searchTerm);
+  };
 
   return(
     <div className="card">
     <h1>🎮 {gameTitle}アプリ</h1>
+
+    <input 
+      type="text"
+      placeholder="ゲーム名を入力"
+      value={searchTerm}
+      onChange={(e) => setSerchTerm(e.target.value)}
+      />
+    <button onClick={handleSearch} className="search-button">
+      検索する
+    </button>
     </div>
   );
   }
