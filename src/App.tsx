532002d197304aca532f726d0bcdbe19108471de
ssk294 <fy69bz45gc@privaterelay.appleid.com>
2026-06-05 13:48:27 +0900

    もう一度最初からスタートするためにコードのお掃除

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import './index.css';
 
 import { useState } from 'react';
 export default function App() {
-  const gameTitle = 'おすすめのゲーム検索🎮';
+  const gameTitle = 'aaaaaaa🎮';
   const [searchTerm, setSerchTerm] = useState('');
 
   const handleSearch = () =>{
@@ -11,17 +11,7 @@ export default function App() {
 
   return(
     <div className="card">
-    <h1>🎮 {gameTitle}アプリ</h1>
-
-    <input 
-      type="text"
-      placeholder="ゲーム名を入力"
-      value={searchTerm}
-      onChange={(e) => setSerchTerm(e.target.value)}
-      className="search-text-card" />
-    <button onClick={handleSearch} className="search-button">
-      検索する
-    </button>
+  
     </div>
   );
   }
