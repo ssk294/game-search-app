ca7fcb7b11b7a64049f35dfd68836263a8d90530
ssk294 <fy69bz45gc@privaterelay.appleid.com>
2026-06-11 18:41:38 +0900

    ゲームのジャンルタグやレビュー価格などの仮置き

--- src/components/FeaturedSection.tsx
+++ src/components/FeaturedSection.tsx
@@ -11,14 +11,32 @@ export default function FeaturedSection(){
                 </div>
 
                 <div className="feature-right">
-                <h3 className="game-title">タイトル</h3>
-                    <ul className="sub-image-container">
-                    <li><img src="/test.png" alt="仮画像" className="sub-image" /></li>
-                    <li><img src="/test.png" alt="仮画像" className="sub-image" /></li>
-                    <li><img src="/test.png" alt="仮画像" className="sub-image" /></li>
-                    <li><img src="/test.png" alt="仮画像" className="sub-image" /></li>
-                    </ul>
+                    <h3 className="game-title">タイトル</h3>
+                        <ul className="sub-image-container">
+                        <li><img src="/test.png" alt="仮画像" className="sub-image" /></li>
+                        <li><img src="/test.png" alt="仮画像" className="sub-image" /></li>
+                        <li><img src="/test.png" alt="仮画像" className="sub-image" /></li>
+                        <li><img src="/test.png" alt="仮画像" className="sub-image" /></li>
+                        </ul>
+
+                        <div className ="game-info-tags">
+                            <div className="game-genre">
+                                <span>アクション</span>
+                                <span>オープンワールド</span>
+                            </div>
+                        </div>
+
+                        <div className ="review-rating">
+                            レビュー:<span className ="rating-status">好評</span>
+                        </div>
+
+                        <div className ="price-display">
+                            <span className ="discount-rate">-50%</span>
+                            <span className ="original-price">￥7600</span>
+                            <span className ="sale-price">￥3800</span>
+                        </div>
                 </div>
+
             </div>
 
         </div>
