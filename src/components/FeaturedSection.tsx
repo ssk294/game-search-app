b4e43ac3d827bb4a613f97ad76ef7617435c5aa0
ssk294 <fy69bz45gc@privaterelay.appleid.com>
2026-06-11 17:53:09 +0900

    スペルミスの訂正

--- src/components/FeaturedSection.tsx
+++ src/components/FeaturedSection.tsx
@@ -1,9 +1,25 @@
 import '../App.css';
 
-export default function FeaturedSectyion(){
+export default function FeaturedSection(){
     return(
-        <div className='fetured-section'>
+        <div className='featured-section'>
+            <h2>あなたへのおすすめ</h2>
             
+            <div className="feature-card">
+                <div className="feature-left">
+                <img src="/test.png" alt="仮画像" className="main-image"/>
+                </div>
+
+                <div className="feature-right">
+                <h3 className="game-title">タイトル</h3>
+                    <ul className="sub-image-container">
+                    <li><img src="/test.png" alt="仮画像" className="sub-image" /></li>
+                    <li><img src="/test.png" alt="仮画像" className="sub-image" /></li>
+                    <li><img src="/test.png" alt="仮画像" className="sub-image" /></li>
+                    <li><img src="/test.png" alt="仮画像" className="sub-image" /></li>
+                    </ul>
+                </div>
+            </div>
 
         </div>
     );
