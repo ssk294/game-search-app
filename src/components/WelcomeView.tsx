7c883c0996473adfafaa96e683383795e3924788
ssk294 <fy69bz45gc@privaterelay.appleid.com>
2026-06-14 12:40:54 +0900

    クラス名訂正

--- src/components/WelcomeView.tsx
+++ src/components/WelcomeView.tsx
@@ -4,7 +4,7 @@ export default function WelcomeView(){
     return(
         <div>
             <div className = "Welcome-container">
-                <p className = 'welcome-text'>
+                <p className = 'Welcome-text'>
                 何万件もの作品から、今のあなたに最高の作品を導き出します。まずは診断してみましょう！
                 </p>
 
