244fec6f4a41943958a79792c7ce4f4c852760a2
ssk294 <fy69bz45gc@privaterelay.appleid.com>
2026-06-14 12:03:07 +0900

    キャッチコピーとボタン配置

--- src/components/WelcomeView.tsx
+++ src/components/WelcomeView.tsx
@@ -3,6 +3,16 @@ import '../App.css';
 export default function WelcomeView(){
     return(
         <div>
+            <div className = "Welcome-container">
+                <p className = 'welcome-text'>
+                何万件もの作品から、今のあなたに最高の作品を導き出します。まずは診断してみましょう！
+                </p>
+
+            <button className = 'start-btn'>
+            START
+            </button>
+
+            </div>
 
         </div>
     );
