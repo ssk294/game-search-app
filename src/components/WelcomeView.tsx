21f8667044e4cd98a15e4dab802ab22ef1d4388b
ssk294 <fy69bz45gc@privaterelay.appleid.com>
2026-06-14 13:27:53 +0900

    テキスト修正

--- src/components/WelcomeView.tsx
+++ src/components/WelcomeView.tsx
@@ -7,7 +7,7 @@ export default function WelcomeView({ onStart }: WelcomeViewProps){
         <div>
             <div className = "Welcome-container">
                 <p className = 'Welcome-text'>
-                何万件もの作品から、今のあなたに最高の作品を導き出します。まずは診断してみましょう！
+                何万件もの作品から、今のあなたに最高のゲーム作品を導き出します。まずは診断してみましょう！
                 </p>
 
             <button className = 'start-btn' onClick={onStart}>
