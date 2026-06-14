ca9a2620e82b104216992788d1fd6ae273df7234
ssk294 <fy69bz45gc@privaterelay.appleid.com>
2026-06-14 12:03:28 +0900

    コード整理とwelcomeviewの呼び出し

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import './index.css';
 import './App.css';
 import FeaturedSection from './components/FeaturedSection';
+import WelcomeView from './components/WelcomeView';
 
 export default function App() {
   return (
     <div>
+      <WelcomeView/>
 
-
-
-          <FeaturedSection />
+      <FeaturedSection />
 
     </div>
   );
