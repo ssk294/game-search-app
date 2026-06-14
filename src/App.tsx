b72d666abe22f68182d4ce6b1e7600f113c12a92
ssk294 <fy69bz45gc@privaterelay.appleid.com>
2026-06-14 11:44:23 +0900

    デザインや方向性を変えるために一度コードお掃除

--- src/App.tsx
+++ src/App.tsx
@@ -6,26 +6,7 @@ export default function App() {
   return (
     <div>
 
-      <header className="header-title">
-        GameStation
-      </header>
-      
-      {/*最初のナビエリア*/}
-      <div className="nav-container">
-        <nav>
-          <ul className="nav-list">
-            <li>おすすめ</li>
-            <li>カテゴリー</li>
-            <li>ハードウェア</li>
-            <li>プレイ手段</li>
-          </ul>
-        </nav>
 
-        <div className="Serch-Box">
-          <input placeholder ="ゲームを検索" className="Serch-Box-design" />
-        </div>
-
-      </div>
 
           <FeaturedSection />
 
