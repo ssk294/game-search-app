2e8ed835713b4743913a67ed72281a94bdb1c4d6
ssk294 <fy69bz45gc@privaterelay.appleid.com>
2026-06-17 15:37:18 +0900

    バーを追加

--- src/components/QuestionView.tsx
+++ src/components/QuestionView.tsx
@@ -5,11 +5,15 @@ interface QuestionViewProps{
 export default function QuestionView({ onStart} :QuestionViewProps){
     return(
         <div className ="Question-Section">
+            <div className ="Progress-bar">
+                <div className ="Progress-bar-fill">test</div>
+            </div>
+
             <h1 className= "Question-text">今の気分は？</h1>
 
             <div className ="Question-container">
-                <button>手軽にプレイしたい</button>
-                <button>じっくりプレイしたい</button>
+                <button className ="Question-btn">手軽にプレイしたい</button>
+                <button className ="Question-btn">じっくりプレイしたい</button>
             </div>
         </div>
     );
