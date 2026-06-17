2587ecdcb49760cd66258ac8839a383129798a56
ssk294 <fy69bz45gc@privaterelay.appleid.com>
2026-06-17 15:42:57 +0900

    ボタンのテスト

--- src/components/QuestionView.tsx
+++ src/components/QuestionView.tsx
@@ -3,6 +3,11 @@ interface QuestionViewProps{
     onStart: () =>void;
 }
 export default function QuestionView({ onStart} :QuestionViewProps){
+
+    const handleAnswerClick = () =>{
+        alert("test");
+    };
+
     return(
         <div className ="Question-Section">
             <div className ="Progress-bar">
@@ -12,8 +17,8 @@ export default function QuestionView({ onStart} :QuestionViewProps){
             <h1 className= "Question-text">今の気分は？</h1>
 
             <div className ="Question-container">
-                <button className ="Question-btn">手軽にプレイしたい</button>
-                <button className ="Question-btn">じっくりプレイしたい</button>
+                <button className ="Question-btn" onClick ={handleAnswerClick} >手軽にプレイしたい</button>
+                <button className ="Question-btn" onClick ={handleAnswerClick} >じっくりプレイしたい</button>
             </div>
         </div>
     );
