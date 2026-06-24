c185ed42c378ce00449ab4338ca783b06149030a
ssk294 <fy69bz45gc@privaterelay.appleid.com>
2026-06-24 10:40:26 +0900

    データの受け取る準備そして仮テスト用のコード

--- src/components/ResultSection.tsx
+++ src/components/ResultSection.tsx
@@ -1,8 +1,11 @@
 import '../App.css';
 interface ResultSectionProps{
     onStart: () =>void;
+    title: string;
+    image: string;
+    desc: string;
 }
-export default function ResultSection({ onStart } :ResultSectionProps){
+export default function ResultSection({ onStart ,title ,image ,desc } :ResultSectionProps){
 
     const handleAnswerClick = () =>{
         onStart();
@@ -11,6 +14,17 @@ export default function ResultSection({ onStart } :ResultSectionProps){
     return(
     <div className ="Result-container">
         <h2>今のあなたに合うゲームはこちら！</h2>
+
+        <div className="Result-card">
+            <h2>{title}</h2>
+
+            <div className ="Result-image-container">
+                <img src={image} alt={title} className ="Result-image" />
+            </div>
+
+            <p className ="Result-desc" >{desc}</p>
+
+        </div>
     </div>
     );
 
