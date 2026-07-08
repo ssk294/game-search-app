27eafc713f472f8bd39da3feb324ed93c656b10d
ssk294 <fy69bz45gc@privaterelay.appleid.com>
2026-07-08 16:23:55 +0900

    三つ分データを受け取れるようにテスト

--- src/components/ResultSection.tsx
+++ src/components/ResultSection.tsx
@@ -1,27 +1,27 @@
 import '../App.css';
-interface ResultSectionProps{
-    onStart: () =>void;
-    title: string;
-    image: string;
-    desc: string;
+interface ResultSectionProps {
+    onStart: () => void;
+    resultGame: any[]; 
 }
-export default function ResultSection({ onStart ,title ,image ,desc } :ResultSectionProps){
 
-    return(
-    <div className ="Result-container">
-        <h2>今のあなたに合うゲームはこちら！</h2>
 
-        <div className="Result-card">
-            <h2>{title}</h2>
+    export default function ResultSection({ onStart, resultGame }: ResultSectionProps) {
+    return (
+        <div className="Result-container">
+            <h2>今のあなたに合うゲームはこちら！</h2>
 
-            <div className ="Result-image-container">
-                <img src={image} alt={title} className ="Result-image" />
-            </div>
+            {resultGame && resultGame.map((game, index) => (
+                <div className="Result-card" key={index}>
+                    <h2>{game.title}</h2>
 
-            <p className ="Result-desc" >{desc}</p>
+                    <div className="Result-image-container">
+                        <img src={game.image} alt={game.title} className="Result-image" />
+                    </div>
 
+                    <p className="Result-desc">{game.desc}</p>
+                </div>
+            ))}
+            
         </div>
-    </div>
     );
-
 }
