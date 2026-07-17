a79d230e4e38ab2c3ee6302482d44f829edfbb14
ssk294 <fy69bz45gc@privaterelay.appleid.com>
2026-07-17 16:16:15 +0900

    レイアウト調整のためにclassNameなどの追加

--- src/components/ResultSection.tsx
+++ src/components/ResultSection.tsx
@@ -8,20 +8,24 @@ interface ResultSectionProps {
     export default function ResultSection({ onStart, resultGame }: ResultSectionProps) {
     return (
         <div className="Result-container">
-            <h2>今のあなたに合うゲームはこちら！</h2>
+            <h2 className="announce-text">今のあなたに合うゲームはこちら！</h2>
 
+            <div className="Result-card-container">
             {resultGame && resultGame.map((game, index) => (
-                <div className="Result-card" key={index}>
-                    <h2>{game.title}</h2>
-
-                    <div className="Result-image-container">
-                        <img src={game.image} alt={game.title} className="Result-image" />
-                    </div>
-
-                    <p className="Result-desc">{game.desc}</p>
-                </div>
-            ))}
             
+                    <div className="Result-card" key={index}>
+                        <h2>{game.title}</h2>
+
+                        <div className="Result-image-container">
+                            <img src={game.image} alt={game.title} className="Result-image" />
+                        </div>
+
+                        <p className="Result-desc">{game.desc}</p>
+                    </div>
+        
+            ))}
+            </div>
+
         </div>
     );
 }
