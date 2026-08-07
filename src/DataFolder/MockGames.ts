2274ba0f256e536c0aad457b366d8a92f366eb2d
ssk294 <fy69bz45gc@privaterelay.appleid.com>
2026-08-07 19:07:13 +0900

    仮のテストモックデータの追加

---
+++ src/DataFolder/MockGames.ts
@@ -0,0 +1,14 @@
+export const MOCK_GAMES = [{
+   name:"サンプルクエスト",
+   background_image:"https://placehold.jp/300*200.png",
+   rating: 4.5,
+   released:"2025-08-12",
+   slug: "sample-quest",
+   genres: [{ id:"5"}], 
+   tags:[
+    { name: "Singleplayer", slug: "singleplayer", language: "eng" },
+    { name: "2D", slug: "2d", language: "eng" },
+   ],
+   platform:[{ platform: {id: 4} }],
+},
+];
