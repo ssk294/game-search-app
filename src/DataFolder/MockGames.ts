d409524f2783f3d1bb2ece86a5bfa1d5fc8be2ad
ssk294 <fy69bz45gc@privaterelay.appleid.com>
2026-08-18 16:59:09 +0900

    モックデータの追加

--- src/DataFolder/MockGames.ts
+++ src/DataFolder/MockGames.ts
@@ -11,4 +11,62 @@ export const MOCK_GAMES = [{
    ],
    platforms:[{ platform: {id: 4} }],
 },
+
+{
+   name:"サンプルシューティング",
+   background_image:"https://placehold.jp/300x200.png",
+   rating: 4.8,
+   released:"2025-06-13",
+   slug: "sample-shooting",
+   genres: [{ id:"2"}], 
+   tags:[
+    { name: "Singleplayer", slug: "singleplayer", language: "eng" },
+    { name: "3D", slug: "3d", language: "eng" },
+   ],
+   platforms:[
+      { platform: {id: 4} },
+      { platform: {id: 187} }
+   ],
+},
+
+{
+   name:"サンプルシティメーカー",
+   background_image:"https://placehold.jp/300x200.png",
+   rating: 4.2,
+   released:"2025-01-28",
+   slug: "sample-city-maker",
+   genres: [{ id:"14"}], 
+   tags:[
+    { name: "Singleplayer", slug: "singleplayer", language: "eng" },
+    { name: "2D", slug: "2d", language: "eng" },
+   ],
+   platforms:[
+      { platform: {id: 4} },
+      { platform: {id: 187} },
+      { platform: {id: 7} },
+      { platform: {id: 3} },
+      { platform: {id: 21} }
+   ],
+},
+
+{
+   name:"サンプルファイター",
+   background_image:"https://placehold.jp/300x200.png",
+   rating: 4.12,
+   released:"2025-09-22",
+   slug: "sample-fighter",
+   genres: [
+   { id:"4"},
+   { id:"6"}
+   ], 
+   tags:[
+    { name: "Multiplayer", slug: "multiplayer", language: "eng" },
+    { name: "3D", slug: "3d", language: "eng" },
+   ],
+   platforms:[
+      { platform: {id: 4} },
+      { platform: {id: 187} },
+   ],
+},
+
 ];
