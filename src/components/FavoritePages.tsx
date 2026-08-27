8297b9a9efd8956138b20d78ada1d77efee88e5b
ssk294 <fy69bz45gc@privaterelay.appleid.com>
2026-08-27 19:53:47 +0900

    タグなどの追加デザイン修正

--- src/components/FavoritePages.tsx
+++ src/components/FavoritePages.tsx
@@ -38,16 +38,26 @@ export default function FavoritePages({ favorites, onRemove, onBack }: FavoriteP
                     <div key={genre.id} className="Favorite-genre-section">
                         <h2>{genre.icon} {genre.name}</h2>
 
+                    <div className="Favorite-scroll-wrapper">
                     <div className="Favorite-scroll-row">
                         {gamesInGenre.map((game) => (
                             <div key={game.title} className="Favorite-card">
                             <p className="Favorite-card-title">{game.title}</p>
                             <img src={game.image} alt={game.title} className="Favorite-card-art" />
+                            <div className="Result-tags-container" >
+                            {game.tags && game.tags.map((tag: string, tagIndex: number) =>(
+                                <span className="Favorite-tag-badge" key={tagIndex}>
+                                    {tag}    
+                                </span>
+                            ))}
+                        </div>
+                            <a href={game.url} className="Favorite-URL" target="_blank" rel= "noopener noreferrer">詳細をみる！</a>
                             <button className="Favorite-remove-btn" onClick={() => onRemove(game.title)}>
                             お気に入りから外す
                             </button>
                             </div>
                         ))}
+                    </div>
                     </div>
                     </div>
                 );
