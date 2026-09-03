88946c4158a44f4334e092cf833e9fe01ad733a4
ssk294 <fy69bz45gc@privaterelay.appleid.com>
2026-09-03 19:17:50 +0900

    色分け用コンポ作成

---
+++ src/components/TagColors.tsx
@@ -0,0 +1,22 @@
+const TAG_CATEGORIES: { keywords: string[]; color: string; bg: string}[] = [
+    {
+        keywords:["Singleplayer", "Multiplayer","Co-op","Online Co-op"],
+        bg: "#EEEDFE",
+        color: "#3C3489",
+    },
+    {
+        keywords:["2D", "3D", "Atomospheric", "Pixel Graphics"],
+        bg: "E1F5EE",
+        color: "#085041",
+    },
+    {
+        keywords: ["Steam Achivements", "Full controller support", "Steam Cloud", "Steam Trading Cards"],
+        bg: "#FBEAFO",
+        color: "#72243E",
+    },
+    {
+        keywords: ["Story Rich", "Great Soundtrack"],
+        bg: "#FFF3D6",
+        color: "7A5300",
+    },
+];
