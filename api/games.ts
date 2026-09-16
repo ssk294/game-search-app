efa9ea77c7040ce13f9a2c6c25aa864fb0be3daf
ssk294 <fy69bz45gc@privaterelay.appleid.com>
2026-09-16 18:14:53 +0900

    システム改善

--- api/games.ts
+++ api/games.ts
@@ -1,7 +1,7 @@
 export default async function handler(req: any, res: any) {
     const apiKey = process.env.RAWG_API_KEY;
   
-    const { genres, tags, ordering, dates, platforms } = req.query;
+    const { genres, tags, ordering, dates, platforms, page } = req.query;
   
     const apiUrl = `https://api.rawg.io/api/games?key=${apiKey}&genres=${genres}&tags=${tags}&ordering=${ordering}&dates=${dates}&platforms=${platforms}&page_size=40`;
   
